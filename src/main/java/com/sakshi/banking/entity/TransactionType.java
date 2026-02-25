3eb27a642ce817ab5c3719938b0648aa1679a6b2
Sakshi <Sakshi35sin@gmail.com>
2026-02-26 00:04:38 +0530

    Added new values

--- src/main/java/com/sakshi/banking/entity/TransactionType.java
+++ src/main/java/com/sakshi/banking/entity/TransactionType.java
@@ -3,7 +3,7 @@ package com.sakshi.banking.entity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum TransactionType {
-    DEPOSIT, TRANSFER, WITHDRAWAL, INTEREST, FEE;
+    DEPOSIT, TRANSFER, WITHDRAWAL, INTEREST, FEE, TRANSFER_DEBIT, TRANSFER_CREDIT;
     @JsonCreator
     public static TransactionType from(String value) {
         return TransactionType.valueOf(value.toUpperCase());
